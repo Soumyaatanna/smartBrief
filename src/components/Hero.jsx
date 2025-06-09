837b02842aa6124883682282bedea8accf21f379
SoumyaTanna <soumyatanna103@gmail.com>
2025-06-09 15:56:24 +0530

    links added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+            window.open("https://github.com/Soumyaatanna/smartBrief", "_blank")
           }
           className='black_btn'
         >
